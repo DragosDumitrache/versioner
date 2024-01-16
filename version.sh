c33de4bf1f1ab525e4d839387311f2008c72db68
Dragos Dumitrache <dragosd2000@gmail.com>
2024-01-16 19:45:24 +0000

    Working on overall improving the code so it's easier to digest
    This should make maintaining and managing much easier

--- version.sh
+++ version.sh
@@ -2,25 +2,27 @@
 
 #source lib.sh
 
-function initialise_version {
+function initialise_version() {
+  # This will initialise your repository for versioner to be able to consume
   if [[ ! -f "version.json" ]]; then
     cat <<EOF >version.json
-  {
-    "default_branch": "master",
-    "major": "0",
-    "minor": "0"
-  }
+    {
+      "default_branch": "master",
+      "major": "0",
+      "minor": "0"
+    }
 EOF
   fi
 }
 
-function is_git_repository {
+# Function to check if the current directory is a git repository
+function is_git_repository() {
   [ -d ".git" ]
 }
 
 function sanitise_version {
-  maybe_starts_with_v=$1
-  no_v=${maybe_starts_with_v#v}
+  raw_version=$1
+  no_v=${raw_version#v}
   no_sha=${no_v%-*}
   if [[ $no_sha != "$no_v" ]]; then
     no_extra_patch=${no_sha%-*}
@@ -54,41 +56,6 @@ function sanitise_version {
   fi
 }
 
-function semver {
-  is_git_repo=$1
-  if [ "$is_git_repo" ]; then
-    echo "1.0.0-SNAPSHOT"
-    return 0
-  fi
-
-  initialise_version
-  git_branch=$(git branch --show-current)
-  if [[ ! $git_branch ]]; then
-    git_branch=$(git rev-parse --abbrev-ref HEAD)
-  fi
-
-  # Get the latest tag
-  latest_tag=$(git tag -l --sort=-creatordate | head -n 1)
-  if [ $latest_tag ]; then
-
-    major_minor=$(echo "$latest_tag" | cut -d '.' -f -2)
-    patch=$(echo "$latest_tag" | cut -d '.' -f 3)
-    # Get the list of commits since the latest tag
-    commits=$(git rev-list --count "$latest_tag"..HEAD)
-    # shellcheck disable=SC2004
-    new_patch=$(($commits + $patch))
-  else
-    new_patch=$patch
-  fi
-  git_generated_version="${major_minor}.${new_patch}"
-  future_major=$(cat version.json | jq ".major" --raw-output)
-  future_minor=$(cat version.json | jq ".minor" --raw-output)
-  default_branch=$(cat version.json | jq ".default_branch" --raw-output)
-
-  final_version=$(calculate_version "$git_branch" "$git_generated_version" "$future_major" "$future_minor" "$default_branch")
-  echo "$final_version"
-}
-
 function calculate_version {
   git_branch=$1
   git_generated_version=$2
@@ -108,6 +75,53 @@ function calculate_version {
 
   echo "$final_version" | tr -d '[:space:]'
 }
+
+function extract_branch_name {
+  git_branch=$(git branch --show-current)
+  if [[ ! $git_branch ]]; then
+    git_branch=$(git rev-parse --abbrev-ref HEAD)
+  fi
+  echo $git_branch
+}
+
+function semver {
+  local git_branch latest_tag major_minor patch commits new_patch  default_branch final_version
+  local next_major next_minor next_patch next_version
+  is_git_repo=$1
+
+  if [ "$is_git_repo" ]; then
+    echo "1.0.0-SNAPSHOT"
+    return 0
+  fi
+
+  # If no version.json file is present, add it, then proceed to calculate the version
+  initialise_version
+
+  git_branch=$(extract_branch_name)
+
+  next_major=$(cat version.json | jq ".major" --raw-output)
+  next_minor=$(cat version.json | jq ".minor" --raw-output)
+  next_patch=0
+  default_branch=$(cat version.json | jq ".default_branch" --raw-output)
+
+  # Get the latest tag
+  latest_tag=$(git tag -l --sort=-creatordate | head -n 1)
+  if [ $latest_tag ]; then
+    major_minor=$(echo "$latest_tag" | cut -d '.' -f -2)
+    patch=$(echo "$latest_tag" | cut -d '.' -f 3)
+    # Get the list of commits since the latest tag
+    commits=$(git rev-list --count "$latest_tag"..HEAD)
+    # shellcheck disable=SC2004
+    next_patch=$(($commits + $patch))
+    next_version="${major_minor}.${next_patch}"
+  else
+    next_version="${next_major}.${next_minor}.${next_patch}"
+  fi
+
+  final_version=$(calculate_version "$git_branch" "$next_version" "$next_major" "$next_minor" "$default_branch")
+  echo "$final_version"
+}
+
 
 if [ "${BASH_SOURCE[0]}" = "$0" ]; then
   semver "$(is_git_repository)"
