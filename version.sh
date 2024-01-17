4e23fb96e891b02754e88362f7a4a012e04f73ab
Dragos Dumitrache <dragosd2000@gmail.com>
2024-01-17 08:18:22 +0000

    Use bats for testing

--- version.sh
+++ version.sh
@@ -2,7 +2,7 @@
 
 #source lib.sh
 
-function initialise_version() {
+function _initialise_version() {
   # This will initialise your repository for versioner to be able to consume
   if [[ ! -f "version.json" ]]; then
     cat <<EOF >version.json
@@ -16,67 +16,41 @@ EOF
 }
 
 # Function to check if the current directory is a git repository
-function is_git_repository() {
+function _is_git_repository() {
   [ -d ".git" ]
 }
 
-function sanitise_version {
-  raw_version=$1
-  no_v=${raw_version#v}
-  no_sha=${no_v%-*}
-  if [[ $no_sha != "$no_v" ]]; then
-    no_extra_patch=${no_sha%-*}
-    major=$(echo "$no_sha" | cut -d '.' -f1)
-    minor=$(echo "$no_sha" | cut -d '.' -f2)
-    patch_number=$(echo "$no_extra_patch" | cut -d '.' -f3)
-    if [[ $major -lt $future_major ]]; then
-      echo "${future_major}.${future_minor}.0"
-    else
-      if [[ $minor -lt $future_minor ]]; then
-        echo "${major}.${future_minor}.0"
-      else
-        git_generated_version="${major}.${minor}.${patch_number}"
-        echo "$git_generated_version"
-      fi
-    fi
-  else
-    major=$(echo "$no_sha" | cut -d '.' -f1)
-    minor=$(echo "$no_sha" | cut -d '.' -f2)
-    patch_number=$(echo "$no_sha" | cut -d '.' -f3)
-    if [[ $major -lt $future_major ]]; then
-      echo "${future_major}.${future_minor}.0"
-    else
-      if [[ $minor -lt $future_minor ]]; then
-        echo "${major}.${future_minor}.0"
-      else
-        git_generated_version="${major}.${minor}.${patch_number}"
-        echo "$git_generated_version"
-      fi
-    fi
-  fi
-}
-
-function calculate_version {
+function _calculate_version {
   git_branch=$1
   git_generated_version=$2
-  future_major=$3
-  future_minor=$4
+  next_major=$3
+  next_minor=$4
   default_branch=$5
   if [[ ! "${git_generated_version}" =~ ^v?.+\..+\.[^-]+ ]]; then
-    git_generated_version="${future_major}.${future_minor}.0"
+    # This case should never happen
+    git_generated_version="${next_major}.${next_minor}.0"
   fi
   final_version=$git_generated_version
   if [[ $git_branch == "$default_branch" ]]; then
-    final_version=$(sanitise_version "$git_generated_version")
+    major=$(echo "$final_version" | cut -d '.' -f1)
+    minor=$(echo "$final_version" | cut -d '.' -f2)
+    patch_number=$(echo "$final_version" | cut -d '.' -f3)
+    if [[ $major -lt $next_major ]]; then
+        final_version="${next_major}.${next_minor}.0"
+    else
+      if [[ $minor -lt $next_minor ]]; then
+        final_version="${major}.${next_minor}.0"
+      else
+        final_version="${major}.${minor}.${patch_number}"
+      fi
+    fi
   else
     final_version="${git_generated_version}-${git_branch}"
-    final_version=${final_version#v}
   fi
-
   echo "$final_version" | tr -d '[:space:]'
 }
 
-function extract_branch_name {
+function _extract_branch_name {
   git_branch=$(git branch --show-current)
   if [[ ! $git_branch ]]; then
     git_branch=$(git name-rev --name-only $(git show -s --format=%H))
@@ -95,9 +69,9 @@ function semver {
   fi
 
   # If no version.json file is present, add it, then proceed to calculate the version
-  initialise_version
+  _initialise_version
 
-  git_branch=$(extract_branch_name)
+  git_branch=$(_extract_branch_name)
 
   next_major=$(cat version.json | jq ".major" --raw-output)
   next_minor=$(cat version.json | jq ".minor" --raw-output)
@@ -118,11 +92,11 @@ function semver {
     next_version="${next_major}.${next_minor}.${next_patch}"
   fi
 
-  final_version=$(calculate_version "$git_branch" "$next_version" "$next_major" "$next_minor" "$default_branch")
+  final_version=$(_calculate_version "$git_branch" "$next_version" "$next_major" "$next_minor" "$default_branch")
   echo "$final_version"
 }
 
 
 if [ "${BASH_SOURCE[0]}" = "$0" ]; then
-  semver "$(is_git_repository)"
+  semver "$(_is_git_repository)"
 fi
